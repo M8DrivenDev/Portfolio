4f9d53327a93e8a7fccf4d9de41fa78aad0df950
M8DrivenDev <m8.driven.dev@gmail.com>
2025-07-14 21:38:16 +0300

    Fix: update hero section text to remove redundancy

--- src/components/home/heroSection/index.tsx
+++ src/components/home/heroSection/index.tsx
@@ -15,10 +15,8 @@ const HeroSection = () => {
       <div className="hero-text">
         <h1>
           M8DrivenDev is a{" "}
-          <span className="text-primary">
-            Full-Stack Web Developer, Software Engineer,
-          </span>{" "}
-          and <span className="text-primary">Content Creator</span>
+          <span className="text-primary">Full-Stack Web Developer</span> and{" "}
+          <span className="text-primary">Software Engineer</span>
         </h1>
         <p>He crafts responsive websites where technologies meet creativity</p>
         <PrimaryBtn onClick={handleContactClick}>Contact me !!</PrimaryBtn>
